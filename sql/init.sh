9f2decb30322ae67a05cd25366e7a1dd22834a0e
yoRyuuuuu <ryugo.yoshioka@fuller.co.jp>
2024-12-08 10:35:56 +0900

    add 4-alter.sql

--- sql/init.sh
+++ sql/init.sh
@@ -35,3 +35,9 @@ gzip -dkc 3-initial-data.sql.gz | mysql -u"$ISUCON_DB_USER" \
 		--host "$ISUCON_DB_HOST" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME"
+
+mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_DB_HOST" \
+		--port "$ISUCON_DB_PORT" \
+		"$ISUCON_DB_NAME" < 4-alter.sql
