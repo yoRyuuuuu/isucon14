9da98282644559de3f52ad94c685bbc3ab95e3c7
yoRyuuuuu <ryugo.yoshioka@fuller.co.jp>
2024-12-08 12:17:24 +0900

    前計算のテーブルを作成

--- sql/4-alter.sql
+++ sql/4-alter.sql
@@ -1,5 +1,40 @@
-CREATE INDEX status_created_at_idx ON ride_statuses(ride_id ,created_at DESC);
+CREATE INDEX status_created_at_idx ON ride_statuses (ride_id, created_at DESC);
 
 CREATE INDEX chair_id_updated_at_idx ON rides (chair_id, updated_at);
 
-CREATE INDEX chair_locations_created_at_idx ON chair_locations(chair_id,created_at DESC);
+DROP TABLE IF EXISTS chair_distance;
+
+CREATE TABLE chair_distance (
+  chair_id VARCHAR(26) NOT NULL COMMENT '椅子ID',
+  total_distance INTEGER NOT NULL COMMENT '移動距離',
+  total_distance_updated_at DATETIME (6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '更新日時'
+);
+
+INSERT INTO
+  chair_distance (
+    chair_id,
+    total_distance,
+    total_distance_updated_at
+  )
+SELECT
+  chair_id,
+  ABS(
+    latitude - LAG (latitude) OVER (
+      PARTITION BY
+        chair_id
+      ORDER BY
+        created_at
+    )
+  ) + ABS(
+    longitude - LAG (longitude) OVER (
+      PARTITION BY
+        chair_id
+      ORDER BY
+        created_at
+    )
+  ) AS distance,
+  created_at
+FROM
+  chair_locations
+GROUP BY
+  chair_id;
