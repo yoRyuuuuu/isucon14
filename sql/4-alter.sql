f07ab91e4f0fb025ce04e1d0d5323ae281897296
yoRyuuuuu <ryugo.yoshioka@fuller.co.jp>
2024-12-08 13:35:16 +0900

    add chair_id_updated_at_idx

--- sql/4-alter.sql
+++ sql/4-alter.sql
@@ -2,6 +2,8 @@ CREATE INDEX status_created_at_idx ON ride_statuses (ride_id, created_at DESC);
 
 CREATE INDEX chair_id_updated_at_idx ON rides (chair_id, updated_at);
 
+CREATE INDEX chair_id_updated_at_idx ON chair_locations (chair_id, created_at DESC);
+
 DROP TABLE IF EXISTS chair_distance;
 
 CREATE TABLE chair_distance (
