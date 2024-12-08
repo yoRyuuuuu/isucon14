af15df638d19a34377bdd1d5f26fc5530b68f44a
yoRyuuuuu <sasoseooo@gmail.com>
2024-12-08 21:45:20 +0900

    マッチングアルゴリズムの修正

--- go/internal_handlers.go
+++ go/internal_handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"errors"
+	"math"
 	"net/http"
 )
 
@@ -20,18 +21,36 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	matched := &Chair{}
+	type chairJoinChairLocation struct {
+		CharID    int `db:"id"`
+		Latitude  int `db:"latitude"`
+		Longitude int `db:"longitude"`
+	}
+
+	possibleChairs := []*chairJoinChairLocation{}
+
+	query := `
+		SELECT c.id, cl.latitude, cl.longitude FROM chairs AS c
+			INNER JOIN chair_locations AS cl ON c.id = cl.chair_id
+			WHERE c.is_active = TRUE
+	`
+
+	if err := db.SelectContext(ctx, possibleChairs, query); err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	var matchedID int
+	minDistance := math.MaxInt
 	empty := false
-	for i := 0; i < 10; i++ {
-		if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
-			if errors.Is(err, sql.ErrNoRows) {
-				w.WriteHeader(http.StatusNoContent)
-				return
-			}
-			writeError(w, http.StatusInternalServerError, err)
+	for _, chair := range possibleChairs {
+		distance := abs(chair.Latitude-ride.PickupLatitude) + abs(chair.Longitude-ride.PickupLongitude)
+		if distance < minDistance {
+			minDistance = distance
+			matchedID = chair.CharID
 		}
 
-		if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
+		if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matchedID); err != nil {
 			writeError(w, http.StatusInternalServerError, err)
 			return
 		}
@@ -44,7 +63,7 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
+	if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matchedID, ride.ID); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
 		return
 	}
