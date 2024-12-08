30086b78ef51d119c76ba5acc582cfef2b2c6dc8
yoRyuuuuu <ryugo.yoshioka@fuller.co.jp>
2024-12-08 12:18:59 +0900

    fix

--- sql/4-alter.sql
+++ sql/4-alter.sql
@@ -18,23 +18,30 @@ INSERT INTO
   )
 SELECT
   chair_id,
-  ABS(
-    latitude - LAG (latitude) OVER (
-      PARTITION BY
-        chair_id
-      ORDER BY
-        created_at
-    )
-  ) + ABS(
-    longitude - LAG (longitude) OVER (
-      PARTITION BY
-        chair_id
-      ORDER BY
-        created_at
-    )
-  ) AS distance,
-  created_at
+  SUM(IFNULL (distance, 0)) AS total_distance,
+  MAX(created_at) AS total_distance_updated_at
 FROM
-  chair_locations
+  (
+    SELECT
+      chair_id,
+      created_at,
+      ABS(
+        latitude - LAG (latitude) OVER (
+          PARTITION BY
+            chair_id
+          ORDER BY
+            created_at
+        )
+      ) + ABS(
+        longitude - LAG (longitude) OVER (
+          PARTITION BY
+            chair_id
+          ORDER BY
+            created_at
+        )
+      ) AS distance
+    FROM
+      chair_locations
+  ) tmp
 GROUP BY
-  chair_id;
+  chair_id
