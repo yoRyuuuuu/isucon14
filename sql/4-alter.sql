e6f130a6acad5f23048de813a10f3e2514e6d727
Yamato Kamijo <yamato.kamijo@fuller.co.jp>
2024-12-08 10:53:11 +0900

    chair_locationsのインデックス作成

--- sql/4-alter.sql
+++ sql/4-alter.sql
@@ -1,3 +1,5 @@
 CREATE INDEX status_created_at_idx ON ride_statuses(ride_id ,created_at DESC);
 
 CREATE INDEX chair_id_updated_at_idx ON rides (chair_id, updated_at);
+
+CREATE INDEX chair_locations_created_at_idx ON chair_locations(chair_id,created_at DESC);
