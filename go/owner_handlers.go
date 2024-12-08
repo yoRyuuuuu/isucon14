6a567ebf46663f3d24a59a9931f2e628a4819c16
yoRyuuuuu <ryugo.yoshioka@fuller.co.jp>
2024-12-08 11:15:28 +0900

    サブクエリを排除

--- go/owner_handlers.go
+++ go/owner_handlers.go
@@ -209,11 +209,7 @@ FROM chairs
        LEFT JOIN (SELECT chair_id,
                           SUM(IFNULL(distance, 0)) AS total_distance,
                           MAX(created_at)          AS total_distance_updated_at
-                   FROM (SELECT chair_id,
-                                created_at,
-                                ABS(latitude - LAG(latitude) OVER (PARTITION BY chair_id ORDER BY created_at)) +
-                                ABS(longitude - LAG(longitude) OVER (PARTITION BY chair_id ORDER BY created_at)) AS distance
-                         FROM chair_locations) tmp
+                   FROM distance
                    GROUP BY chair_id) distance_table ON distance_table.chair_id = chairs.id
 WHERE owner_id = ?
 `, owner.ID); err != nil {
