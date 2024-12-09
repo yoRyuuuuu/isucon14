163966a329b12767a6446360b600aef710cab18c
yoRyuuuuu <sasoseooo@gmail.com>
2024-12-09 23:07:52 +0900

    fix

--- go/internal_handlers.go
+++ go/internal_handlers.go
@@ -11,36 +11,27 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	// MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
 	ride := &Ride{}
-	if err := db.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1`); err != nil {
+	matched := &Chair{}
+
+	// Combine the selection of an available ride and chair into a single query
+	query := `
+		SELECT r.id AS ride_id, c.id AS chair_id
+		FROM rides r
+		JOIN chairs c ON c.is_active = TRUE
+		LEFT JOIN ride_statuses rs ON rs.ride_id = r.id
+		WHERE r.chair_id IS NULL
+		GROUP BY r.id, c.id
+		HAVING COUNT(rs.chair_sent_at) < 6
+		ORDER BY r.created_at, RAND()
+		LIMIT 1
+	`
+	row := db.QueryRowContext(ctx, query)
+	if err := row.Scan(&ride.ID, &matched.ID); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			w.WriteHeader(http.StatusNoContent)
 			return
 		}
 		writeError(w, http.StatusInternalServerError, err)
-		return
-	}
-
-	matched := &Chair{}
-	empty := false
-	for i := 0; i < 10; i++ {
-		if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
-			if errors.Is(err, sql.ErrNoRows) {
-				w.WriteHeader(http.StatusNoContent)
-				return
-			}
-			writeError(w, http.StatusInternalServerError, err)
-		}
-
-		if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
-			writeError(w, http.StatusInternalServerError, err)
-			return
-		}
-		if empty {
-			break
-		}
-	}
-	if !empty {
-		w.WriteHeader(http.StatusNoContent)
 		return
 	}
 
